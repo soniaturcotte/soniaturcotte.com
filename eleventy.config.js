7d43ee9076c4d91a6ddce8b7912ecf73be94f100
Jon Heslop <jon@jonheslop.com>
2023-03-14 16:44:14 +0000

    Add all the 11ty blog helpers

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,71 @@
-module.exports = function(eleventyConfig) {
-    // Output directory: _site
-  
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("fonts");
-    eleventyConfig.addPassthroughCopy('**/*.css');
-    eleventyConfig.addPassthroughCopy('favicon.ico');
-} 
+const { DateTime } = require('luxon');
+const markdownItAnchor = require('markdown-it-anchor');
+
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginBundle = require('@11ty/eleventy-plugin-bundle');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
+
+module.exports = function (eleventyConfig) {
+  // Output directory: _site
+
+  eleventyConfig.addPassthroughCopy('images');
+  eleventyConfig.addPassthroughCopy('fonts');
+  eleventyConfig.addPassthroughCopy('main.css');
+  eleventyConfig.addPassthroughCopy('favicon.ico');
+
+  // Official plugins
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginSyntaxHighlight, {
+    preAttributes: { tabindex: 0 },
+  });
+  eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPlugin(pluginBundle);
+
+  // Filters
+  eleventyConfig.addFilter('readableDate', (dateObj, format, zone) => {
+    // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+    return DateTime.fromJSDate(dateObj, { zone: zone || 'utc' }).toFormat(
+      format || 'dd LLLL yyyy'
+    );
+  });
+
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (!Array.isArray(array) || array.length === 0) {
+      return [];
+    }
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  // Return the smallest number argument
+  eleventyConfig.addFilter('min', (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
+
+  // Return all the tags used in a collection
+  eleventyConfig.addFilter('getAllTags', (collection) => {
+    let tagSet = new Set();
+    for (let item of collection) {
+      (item.data.tags || []).forEach((tag) => tagSet.add(tag));
+    }
+    return Array.from(tagSet);
+  });
+
+  eleventyConfig.addFilter('filterTagList', function filterTagList(tags) {
+    return (tags || []).filter(
+      (tag) => ['all', 'nav', 'post', 'posts'].indexOf(tag) === -1
+    );
+  });
+};
