8874f63a6667b586f1eeb251f408395b048fd3de
Jon Heslop <jon@jonheslop.com>
2023-03-14 16:51:59 +0000

    Add RSS feed stuff

---
+++ _data/metadata.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Sonia Turcotte',
+  url: 'https://soniaturcotte.com/',
+  language: 'en',
+  description:
+    'Freelance designer and researcher, interested in building more equitable, abundant futures. Also at Goldsmiths, studying ecology. ',
+  author: {
+    name: 'Sonia Turcotte',
+    email: 'youremailaddress@example.com',
+    url: 'https://soniaturcotte.com/',
+  },
+};
