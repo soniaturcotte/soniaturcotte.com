c20b3dd292c8256a71a22b6a1f64cc9fecafe33e
Sonia Turcotte <sonia.turcotte@gmail.com>
2023-03-14 15:44:08 +0000

    update so css and images are pulled through

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,8 @@
 module.exports = function(eleventyConfig) {
     // Output directory: _site
   
-    // Copy `img/` to `_site/img`
-    eleventyConfig.addPassthroughCopy("img");
-  
-    // Copy `css/fonts/` to `_site/css/fonts`
-    // Keeps the same directory structure.
-    eleventyConfig.addPassthroughCopy("css/fonts");
-  
-    // Copy any .jpg file to `_site`, via Glob pattern
-    // Keeps the same directory structure.
-    eleventyConfig.addPassthroughCopy("**/*.jpg");
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("fonts");
+    eleventyConfig.addPassthroughCopy('**/*.css');
+    eleventyConfig.addPassthroughCopy('favicon.ico');
   };
